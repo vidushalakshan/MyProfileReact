8f4cac79c2100335fc9135f277e4e9425716a161
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-04-30 16:58:23 +0530

    updated project some content

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Project.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const Project = () => {
@@ -9,18 +9,25 @@ const Project = () => {
       <h1 className="projectTitle">
         <dfn className="project">My Projects</dfn>
       </h1>
-
-      <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      className="swiperProject"
-    >
-      <SwiperSlide className="hotel"><div className="java"></div></SwiperSlide>
-      <SwiperSlide className="shop"><div className="java"></div></SwiperSlide>
-      <SwiperSlide className="market"><div className="java"></div></SwiperSlide>
-    </Swiper>
+      <section className="section">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="swiperProject"
+        >
+          <SwiperSlide className="hotel">
+            <div className="java"></div>
+          </SwiperSlide>
+          <SwiperSlide className="shop">
+            <div className="java"></div>
+          </SwiperSlide>
+          <SwiperSlide className="market">
+            <div className="java"></div>
+          </SwiperSlide>
+        </Swiper>
+      </section>
     </>
   );
 };
