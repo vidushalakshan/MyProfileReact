8e06b5e4ecdcc4207ef568d43d6996815b9d9c72
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-05-10 12:20:55 +0530

    complete laptop size responsive

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "../components/navbar/NavBar";
 import Home from "./Home";
-import "../styles/Main.css"
+import "../styles/Main.css";
 import AboutMe from "./AboutMe";
 import Skils from "./Skils";
 import Project from "./Project";
@@ -10,34 +10,37 @@ import Contact from "./Contact";
 import Footer from "../components/footer/Footer";
 
 const Main = () => {
-    return (
-        <div className="body">
-            <div>
-                <NavBar/>
-            </div>
-            <div className="home">
-                <Home/>
-            </div>
-            <div >
-                <AboutMe/>
-            </div>
-            <div>
-                <Skils/>
-            </div>
-            <div>
-                <Project/>
-            </div>
-            <div>
-                <Service/>
-            </div>
-            <div>
-                <Contact/>
-            </div>
-            <div>
-                <Footer/>
-            </div>
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div className="home">
+        <Home />
+      </div>
+      <div className="body">
+        <div>
+          <AboutMe />
         </div>
-    )
-}
+        <div>
+          <Skils />
+        </div>
+        <div>
+          <Project />
+        </div>
+        <div>
+          <Service />
+        </div>
+
+        <div>
+          <Contact />
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
